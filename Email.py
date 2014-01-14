43f1bf2dedc3fd23ee3c1e2f1b3b6a0f4ffd7e38
Jonathan Bondhus <jonathan.bondhus@gmail.com>
2014-01-13 20:53:00 -0600

    Initial commit

---
+++ Email.py
@@ -0,0 +1,116 @@
+# Import smtplib for the actual sending functionality
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+from email.Message import Message
+
+# Version Information (follows semantic versioning - )
+# 1.0 - The initial release candidate (versions before this were undocumented)
+
+# This class defines email objects. They are constructed with the server and sender information, and sent with the send method
+class Email:
+    def __init__(self, from_name, from_address, hostname, username, password, priority=3, tls_enabled=True):
+        """
+        Constructs an email object
+
+        Parameters:
+            from_name (str) - The name that will be displayed in the from information (e.g. "John Doe")
+            from_address (str) - The email address to send the email from (e.g. "you@domain.com")
+            hostname  (str) - The hostname of the SMTP server to send the message through (e.g. "smtp.gmail.com:587" for Gmail)
+            username  (str) - The username to authenticate to the SMTP server with (e.g. "you@domain.com")
+            password  (str) - The password to authenticate to the SMTP server with
+            [priority] (int) - The priority of the message, from 1 to 5, where 1 is the highest, 3 is normal, and 5 is the lowest - default is 3
+            [tls_enabled] (bool) - Whether to enable or disable TLS encryption - default is enabled
+        """
+
+        # Make sure the variables are the correct type
+        if not isinstance(hostname, str):
+            raise TypeError("The hostname must be a string")
+        if not isinstance(from_address, str):
+            raise TypeError("The from address must be a string")
+        if not isinstance(priority, int) or priority < 0 or priority > 5:
+            raise TypeError("The priority must be an integer between 0 and 5")
+        if not isinstance(tls_enabled, bool):
+            raise TypeError("Whether tls is enabled must be a boolean value")
+
+        self.hostname = str(hostname)
+        self.from_name = str(from_name)
+        self.from_address = str(from_address)
+        self.username = str(username)
+        self.password = str(password)
+        self.priority = int(priority)
+        self.tls_enabled = bool(tls_enabled)
+
+    def send(self, to_address, subject, message):
+        """
+        Generates and sends the email with the parameters specified
+
+        Parameters:
+            to_address (str) - The email address to send the (e.g. "target@domain.com")
+            subject (str) - The subject of the email message
+            message (str) - The email message data
+        """
+
+        # Make sure the variables are the correct type
+        if not isinstance(to_address, str):
+            raise TypeError("The destination address must be a string")
+
+        subject = str(subject)
+        message = str(message)
+
+        # Create a new email message object
+        m = Message()
+
+        # Set the message object's attributes
+        m['From'] = str(self.from_name)
+        m['X-Priority'] = str(self.priority)
+        m['Subject'] = str(subject)
+
+        # Set the payload (message) for the message object
+        m.set_payload(message)
+
+        # Try to connect to the server
+        try:
+            server = smtplib.SMTP(self.hostname, timeout=5)
+        except:
+            self.write_message("Please check your hostname settings!")
+            sys.exit(1)
+
+        # Greet the server
+        server.ehlo()
+
+        # Check for and enable TLS if requested
+        if self.tls_enabled:
+            try:
+                server.starttls()
+            except:
+                self.write_message("TLS failed to start. Proceeding without encryption!")
+        try: # Try to login, if it fails, write an error message and raise an exception
+            server.login(self.username, self.password)
+        except Exception, e:
+            self.write_message("Server login failed, please check username and password!")
+            raise e
+
+        # Try to send the email, if it fails, write an error message and raise an exception
+        try:
+            server.sendmail(str(self.from_address), str(to_address), m.as_string())
+        except Exception, e:
+            self.write_message("Sending mail failed!")
+            raise e
+
+        server.close()
+
+        # Inform the user that the message has been sent
+        print("Sucessfully sent email. From: " + str(self.from_address) + ", To: " + str(to_address) + ", Subject: " + str(subject))
+
+    def write_message(message):
+        """
+        Right now this prints a message to stdout, but I plan to have it log in the future
+
+        Arguments:
+            message (str) - The message to display
+        """
+
+        print(message)
+
---
+++ class_watcher.py
@@ -0,0 +1,204 @@
+#!/usr/bin/python
+
+__title__ = "Class Monitoring Bot" # Emails the specified email when a class has a free spot
+__author__ = "Jonathan Bondhus"
+__version__ = "1.3 beta"
+
+## Version Information
+# 0.1 alpha - Initial prototype
+# 0.2 alpha - Simplified some programming, and downgraded from BeautifulSoup 4 to BeautifulSoup 3 because of bugs
+# 1.0 beta - Complete overhaul of program structure, re-wrote almost from the ground up
+# 1.1 beta - Minor syntax clean-ups and more commenting added. Moved imports into a try-except block so that any errors
+#			 have a much higher chance of being reported (the modules my email module uses are standard python modules,
+#			 so withstanding a server error or corrupted python installation they should always be installed)
+# 1.2 beta - Added tracebacks to email reports and modified email reporting method parameters
+# 1.3 beta - Fixed bugs with searching for the course name, improved reliability of matches
+
+## TODO
+# - Add persistant storage for last error so that multiple error messages aren't sent repeatedly upon error
+
+## Important Information - Please Read
+# This program must be as fail-safe as possible, so import the modules in a try-except (with the email module going
+# first) so that unless there is an error with the email module errors can be reported via email. Another important
+# thing is thathe __title__ variable must be set in order for this program to function (it's at the beginning of the
+# program)
+
+######################### START CONFIGURATION ###########################
+
+# The URL to scrape the information from
+search_url = "https://banner.stthomas.edu/pls/banner/prod/bwckschd.p_disp_listcrse?term_in=201420&subj_in=CISC&crse_in=210&crn_in=20807"
+
+# The path to the persistant storage database (this may be absolute or relative, but the program should (haven't tested this) throw an exception if it's read-only)
+database_path = "/usr/local/bin/class_watcher/CISC-210/persistent.db"
+
+# Email Settings
+
+# The server settings
+hostname = "smtp.gmail.com:587" # The SMTP server hostname
+tls_enabled = True    # Whether or not TLS is enabled
+
+# The email username and password
+username = "jonathan.bondhus@gmail.com"
+password = "qiVBwGoHpLv4MV8MlB46i7PW76B7qPuS1VEEgmM9zF1JLVDjNMMngbgyJTODl4JBChtr42YOw7tBHQ9XjJZn0juqPVJdKsX7GvEa"
+
+from_name = __title__ # The name to appear in the from section
+from_address = "jonathan.bondhus@gmail.com" # The address the email message is being sent from
+to_address = "jonathan.bondhus@gmail.com" # The address the email message is being sent to
+
+priority = 1 # The priority of the message (1 is the highest, 3 is normal, 5 is the lowest)
+
+########################## END CONFIGURATION ############################
+
+def report_error(exception, user_message="Something has gone wrong, debugging information follows:\n\n"):
+	"""
+	Reports any errors that arise
+
+	Defaults:
+		user_message = "Something has gone wrong, debugging information follows:\n\n"
+
+	Parameters:
+		exception (Exception) - The exception object to report
+		user_message (str) - An optional message to include (If included, this will override the default message)
+	"""
+
+	message = str(user_message) + traceback.format_exc()
+	
+	# Make sure that the emailer is initialized, this method MUST not fail
+	try:
+		emailer
+	except:
+		emailer = Email(from_name, from_address, hostname, username, password, priority)
+
+	write_message("An error has occured, an attempt will be made to email the information to the address you specified")
+
+	try: # Try to send the email
+		emailer.send(to_address, __title__ + " Error", message)
+	except Exception, e: # If sending the email fails, write an error message to the screen - logging will be implemented later
+		write_message("Sending the error email failed!")
+		raise e
+
+def initialize():
+	"""
+	Initializes the global variables
+	"""
+
+	# Declare global the variables that must be global
+	global emailer
+	global root
+
+	# Initialize the email object first so that we can send an error message if something breaks
+	emailer = Email(from_name, from_address, hostname, username, password, priority)
+
+	# Initialize the persistant database
+	storage = FileStorage.FileStorage(database_path)
+	db = DB(storage)
+	connection = db.open()
+	root = connection.root()
+
+	# Make sure the section info object is initialized
+	try:
+		root['section_info']
+	except:
+		root['section_info'] = Section(search_url)
+		root['section_info'].update()
+		transaction.commit()
+
+	# Make sure the URL hasn't changed
+	if not root['section_info'].url_equals(search_url):
+		root['section_info'] = Section(search_url)
+		root['section_info'].update()
+		transaction.commit()
+		# If it has, update the database, notify the user, and quit
+		emailer.send(to_address, "The course being watched has been changed!", "The course being watched has been "
+			+ "changed to " + root['section_info'].get_course_name())
+		sys.exit()
+
+def main():
+	# Most exceptions will be handled and reported by the main method
+	try:
+		initialize() # Initialize the persistent variables and the emailer
+
+		# Save whether the section is open before we update
+		pre_update_is_section_open = root['section_info'].is_section_open()
+
+		root['section_info'].update() # Update the section information
+
+		# After the section has been updated, save the new information about whether or not it is open
+		post_update_is_section_open = root['section_info'].is_section_open()
+
+		# If the section has changed, notify the user, otherwise, print a message and exit
+		if (pre_update_is_section_open != post_update_is_section_open):
+			notify(post_update_is_section_open, root['section_info'].get_course_name())
+		else:
+			write_message("Nothing has changed, not emailing")
+			sys.exit()
+
+		# Commit the transaction
+		transaction.commit()
+
+	except Exception, e: # If an error occurs anywhere, report it
+		report_error(e)
+		raise e
+
+def notify(is_section_open, course_name):
+	"""
+	Notifies the user whether the section status has changed
+
+	Parameters:
+		is_section_open (bool) - Whether or not the section to notify the user about is open
+		course_name (str) - The name of the course no notify the user about
+	"""
+
+	if is_section_open:
+		subject = "The course " + course_name + " has opened for registration!" # The subject of the email message
+		message = "Go to the following link to view it and verify: \n\n" + search_url
+	else:
+		subject = "The course " + course_name + " has closed for registration!" # The subject of the email message
+		message = "Go to the following link to view it and verify: \n\n" + search_url	
+	emailer.send(to_address, subject, message)
+
+def write_message(message):
+	"""
+	Right now this prints a message to stdout, but I plan to have it log in the future
+
+	Arguments:
+		message (str) - The message to display
+
+	Returns: Nothing
+	"""
+
+	print(str(message))
+
+# Try importing the required modules, attempt to email the error if something goes wrong
+try:
+	# Import the traceback module for retrieving tracback information from exceptions
+	import traceback
+
+	# Import my email module - this must be done first along with traceback (which is needed by email so it must be
+	# before email) so if any errors happen importing the rest of the modules they are reported
+	from Email import Email
+
+	# Some BS to shut up the interpreter
+	import logging
+	logging.basicConfig()
+	
+	# Import system modules (mainly used for exiting the program)
+	import os
+	import sys
+	
+	# Import modules for the persistant object database
+	from ZODB import FileStorage, DB
+	import transaction
+	
+	# Import my section information module
+	from Section import Section
+except Exception, e:
+	try:
+		report_error(e)
+	except NameError, e:
+		write_message("The email module must not have been able to been imported!")
+	raise e
+
+main()
+sys.exit() # Exit the program
+
