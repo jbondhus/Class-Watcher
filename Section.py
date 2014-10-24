f3194ac459043cca71660083df8b8e6109019cde
Jonathan Bondhus <jonathan.bondhus@gmail.com>
2014-10-23 23:19:42 -0500

    Converted indentation to spaces

--- Section.py
+++ Section.py
@@ -12,87 +12,87 @@ from persistent import Persistent
 
 # Defines course section objects
 class Section(Persistent):
-	def __init__(self, search_url):
-		"""
-		Generates a new section object with the url to pull the information from as a parameter
+    def __init__(self, search_url):
+        """
+        Generates a new section object with the url to pull the information from as a parameter
 
-		Parameters:
-			search_url (str) - The URL to pull the section (course) information from
-		"""
+        Parameters:
+            search_url (str) - The URL to pull the section (course) information from
+        """
 
-		# Make sure the variables are the correct type
-		if not isinstance(search_url, str):
-			raise TypeError("The search url must be a string")
+        # Make sure the variables are the correct type
+        if not isinstance(search_url, str):
+            raise TypeError("The search url must be a string")
 
-		try:
-			requests.get(search_url, timeout=60)
-		except Exception as e:
-			raise ValueError("Please check that it is possible to connect to the URL you specified!")
+        try:
+            requests.get(search_url, timeout=60)
+        except Exception as e:
+            raise ValueError("Please check that it is possible to connect to the URL you specified!")
 
-		self.search_url = str(search_url)
-		self.html = None
-		self.soup = None
+        self.search_url = str(search_url)
+        self.html = None
+        self.soup = None
 
-	def update(self):
-		"""
-		Updates the HTML
-		"""
+    def update(self):
+        """
+        Updates the HTML
+        """
 
-		response = requests.get(self.search_url, timeout=5) # Query the search url
-		self.html = response.text # Get the html from the response
-		self.soup = BeautifulSoup(self.html) # Parse the HTML to create a BeautifulSoup object (similar to a parse tree)
+        response = requests.get(self.search_url, timeout=5) # Query the search url
+        self.html = response.text # Get the html from the response
+        self.soup = BeautifulSoup(self.html) # Parse the HTML to create a BeautifulSoup object (similar to a parse tree)
 
-	def is_section_open(self):
-		"""
-		Gets whether the section is open from the html
-		"""
+    def is_section_open(self):
+        """
+        Gets whether the section is open from the html
+        """
 
-		# Make sure the HTML instance variable isn't null
-		if (self.soup == None):
-			self.update()
+        # Make sure the HTML instance variable isn't null
+        if (self.soup == None):
+            self.update()
 
-		# If the section status isn't found, throw an error, otherwise return the status
-		if (self.soup).body.find("b", text="Section Closed") != None:
-			return False
-		elif (self.soup).body.find("b", text="Open") != None:
-			return True
-		else:
-			raise Exception("The status of the section could not be found!")
+        # If the section status isn't found, throw an error, otherwise return the status
+        if (self.soup).body.find("b", text="Section Closed") != None:
+            return False
+        elif (self.soup).body.find("b", text="Open") != None:
+            return True
+        else:
+            raise Exception("The status of the section could not be found!")
 
-	def get_course_name(self):
-		"""
-		Gets the course name from the html
-		"""
+    def get_course_name(self):
+        """
+        Gets the course name from the html
+        """
 
-		# Make sure the soup instance variable isn't null
-		if (self.soup == None):
-			self.update()
+        # Make sure the soup instance variable isn't null
+        if (self.soup == None):
+            self.update()
 
-		# The absolute name (taken straight from the HTML)
-		absolute_name = str(self.soup.find("th", "ddtitle").a.contents[0])
+        # The absolute name (taken straight from the HTML)
+        absolute_name = str(self.soup.find("th", "ddtitle").a.contents[0])
 
-		# The formatted name (parsed with a regular expression from the HTML)
-		queried_name = re.search('[A-Z]{4}\s[0-9]{3}', absolute_name).group()
+        # The formatted name (parsed with a regular expression from the HTML)
+        queried_name = re.search('[A-Z]{4}\s[0-9]{3}', absolute_name).group()
 
-		if (queried_name != None): # If we aren't able to format it, set the class name to the absolute name (the one
-								   # before formatting) as a fail-over
-			course_name = queried_name
-		else:
-			course_name = absolute_name
+        if (queried_name != None): # If we aren't able to format it, set the class name to the absolute name (the one
+                                   # before formatting) as a fail-over
+            course_name = queried_name
+        else:
+            course_name = absolute_name
 
-		return course_name		
+        return course_name      
 
-	def url_equals(self, url):
-		"""
-		Checks whether the url passed equals the saved url of the section
+    def url_equals(self, url):
+        """
+        Checks whether the url passed equals the saved url of the section
 
-		Parameters:
-			url (str) - The URL to compare
-		"""
+        Parameters:
+            url (str) - The URL to compare
+        """
 
-		# Make sure the variables are the correct type
-		if not isinstance(url, str):
-			raise TypeError("The url must be a string")
+        # Make sure the variables are the correct type
+        if not isinstance(url, str):
+            raise TypeError("The url must be a string")
 
-		return self.search_url == str(url)
+        return self.search_url == str(url)
 
--- class_watcher.py
+++ class_watcher.py
@@ -9,8 +9,8 @@ __version__ = "1.3 beta"
 # 0.2 alpha - Simplified some programming, and downgraded from BeautifulSoup 4 to BeautifulSoup 3 because of bugs
 # 1.0 beta - Complete overhaul of program structure, re-wrote almost from the ground up
 # 1.1 beta - Minor syntax clean-ups and more commenting added. Moved imports into a try-except block so that any errors
-#			 have a much higher chance of being reported (the modules my email module uses are standard python modules,
-#			 so withstanding a server error or corrupted python installation they should always be installed)
+#            have a much higher chance of being reported (the modules my email module uses are standard python modules,
+#            so withstanding a server error or corrupted python installation they should always be installed)
 # 1.2 beta - Added tracebacks to email reports and modified email reporting method parameters
 # 1.3 beta - Fixed bugs with searching for the course name, improved reliability of matches
 
@@ -50,154 +50,154 @@ priority = 1 # The priority of the message (1 is the highest, 3 is normal, 5 is 
 ########################## END CONFIGURATION ############################
 
 def report_error(exception, user_message="Something has gone wrong, debugging information follows:\n\n"):
-	"""
-	Reports any errors that arise
+    """
+    Reports any errors that arise
 
-	Defaults:
-		user_message = "Something has gone wrong, debugging information follows:\n\n"
+    Defaults:
+        user_message = "Something has gone wrong, debugging information follows:\n\n"
 
-	Parameters:
-		exception (Exception) - The exception object to report
-		user_message (str) - An optional message to include (If included, this will override the default message)
-	"""
+    Parameters:
+        exception (Exception) - The exception object to report
+        user_message (str) - An optional message to include (If included, this will override the default message)
+    """
 
-	message = str(user_message) + traceback.format_exc()
-	
-	# Make sure that the emailer is initialized, this method MUST not fail
-	try:
-		emailer
-	except:
-		emailer = Email(from_name, from_address, hostname, username, password, priority)
+    message = str(user_message) + traceback.format_exc()
+    
+    # Make sure that the emailer is initialized, this method MUST not fail
+    try:
+        emailer
+    except:
+        emailer = Email(from_name, from_address, hostname, username, password, priority)
 
-	write_message("An error has occured, an attempt will be made to email the information to the address you specified")
+    write_message("An error has occured, an attempt will be made to email the information to the address you specified")
 
-	try: # Try to send the email
-		emailer.send(to_address, __title__ + " Error", message)
-	except Exception as e: # If sending the email fails, write an error message to the screen - logging will be implemented later
-		write_message("Sending the error email failed!")
-		raise e
+    try: # Try to send the email
+        emailer.send(to_address, __title__ + " Error", message)
+    except Exception as e: # If sending the email fails, write an error message to the screen - logging will be implemented later
+        write_message("Sending the error email failed!")
+        raise e
 
 def initialize():
-	"""
-	Initializes the global variables
-	"""
+    """
+    Initializes the global variables
+    """
 
-	# Declare global the variables that must be global
-	global emailer
-	global root
+    # Declare global the variables that must be global
+    global emailer
+    global root
 
-	# Initialize the email object first so that we can send an error message if something breaks
-	emailer = Email(from_name, from_address, hostname, username, password, priority)
+    # Initialize the email object first so that we can send an error message if something breaks
+    emailer = Email(from_name, from_address, hostname, username, password, priority)
 
-	# Initialize the persistant database
-	storage = FileStorage.FileStorage(database_path)
-	db = DB(storage)
-	connection = db.open()
-	root = connection.root()
+    # Initialize the persistant database
+    storage = FileStorage.FileStorage(database_path)
+    db = DB(storage)
+    connection = db.open()
+    root = connection.root()
 
-	# Make sure the section info object is initialized
-	try:
-		root['section_info']
-	except:
-		root['section_info'] = Section(search_url)
-		root['section_info'].update()
-		transaction.commit()
+    # Make sure the section info object is initialized
+    try:
+        root['section_info']
+    except:
+        root['section_info'] = Section(search_url)
+        root['section_info'].update()
+        transaction.commit()
 
-	# Make sure the URL hasn't changed
-	if not root['section_info'].url_equals(search_url):
-		root['section_info'] = Section(search_url)
-		root['section_info'].update()
-		transaction.commit()
-		# If it has, update the database, notify the user, and quit
-		emailer.send(to_address, "The course being watched has been changed!", "The course being watched has been "
-			+ "changed to " + root['section_info'].get_course_name())
-		sys.exit()
+    # Make sure the URL hasn't changed
+    if not root['section_info'].url_equals(search_url):
+        root['section_info'] = Section(search_url)
+        root['section_info'].update()
+        transaction.commit()
+        # If it has, update the database, notify the user, and quit
+        emailer.send(to_address, "The course being watched has been changed!", "The course being watched has been "
+            + "changed to " + root['section_info'].get_course_name())
+        sys.exit()
 
 def main():
-	# Most exceptions will be handled and reported by the main method
-	try:
-		initialize() # Initialize the persistent variables and the emailer
+    # Most exceptions will be handled and reported by the main method
+    try:
+        initialize() # Initialize the persistent variables and the emailer
 
-		# Save whether the section is open before we update
-		pre_update_is_section_open = root['section_info'].is_section_open()
+        # Save whether the section is open before we update
+        pre_update_is_section_open = root['section_info'].is_section_open()
 
-		root['section_info'].update() # Update the section information
+        root['section_info'].update() # Update the section information
 
-		# After the section has been updated, save the new information about whether or not it is open
-		post_update_is_section_open = root['section_info'].is_section_open()
+        # After the section has been updated, save the new information about whether or not it is open
+        post_update_is_section_open = root['section_info'].is_section_open()
 
-		# If the section has changed, notify the user, otherwise, print a message and exit
-		if (pre_update_is_section_open != post_update_is_section_open):
-			notify(post_update_is_section_open, root['section_info'].get_course_name())
-		else:
-			write_message("Nothing has changed, not emailing")
-			sys.exit()
+        # If the section has changed, notify the user, otherwise, print a message and exit
+        if (pre_update_is_section_open != post_update_is_section_open):
+            notify(post_update_is_section_open, root['section_info'].get_course_name())
+        else:
+            write_message("Nothing has changed, not emailing")
+            sys.exit()
 
-		# Commit the transaction
-		transaction.commit()
+        # Commit the transaction
+        transaction.commit()
 
-	except Exception as e: # If an error occurs anywhere, report it
-		report_error(e)
-		raise e
+    except Exception as e: # If an error occurs anywhere, report it
+        report_error(e)
+        raise e
 
 def notify(is_section_open, course_name):
-	"""
-	Notifies the user whether the section status has changed
+    """
+    Notifies the user whether the section status has changed
 
-	Parameters:
-		is_section_open (bool) - Whether or not the section to notify the user about is open
-		course_name (str) - The name of the course no notify the user about
-	"""
+    Parameters:
+        is_section_open (bool) - Whether or not the section to notify the user about is open
+        course_name (str) - The name of the course no notify the user about
+    """
 
-	if is_section_open:
-		subject = "The course " + course_name + " has opened for registration!" # The subject of the email message
-		message = "Go to the following link to view it and verify: \n\n" + search_url
-	else:
-		subject = "The course " + course_name + " has closed for registration!" # The subject of the email message
-		message = "Go to the following link to view it and verify: \n\n" + search_url	
-	emailer.send(to_address, subject, message)
+    if is_section_open:
+        subject = "The course " + course_name + " has opened for registration!" # The subject of the email message
+        message = "Go to the following link to view it and verify: \n\n" + search_url
+    else:
+        subject = "The course " + course_name + " has closed for registration!" # The subject of the email message
+        message = "Go to the following link to view it and verify: \n\n" + search_url   
+    emailer.send(to_address, subject, message)
 
 def write_message(message):
-	"""
-	Right now this prints a message to stdout, but I plan to have it log in the future
+    """
+    Right now this prints a message to stdout, but I plan to have it log in the future
 
-	Arguments:
-		message (str) - The message to display
+    Arguments:
+        message (str) - The message to display
 
-	Returns: Nothing
-	"""
+    Returns: Nothing
+    """
 
-	print(str(message))
+    print(str(message))
 
 # Try importing the required modules, attempt to email the error if something goes wrong
 try:
-	# Import the traceback module for retrieving tracback information from exceptions
-	import traceback
+    # Import the traceback module for retrieving tracback information from exceptions
+    import traceback
 
-	# Import my email module - this must be done first along with traceback (which is needed by email so it must be
-	# before email) so if any errors happen importing the rest of the modules they are reported
-	from Email import Email
+    # Import my email module - this must be done first along with traceback (which is needed by email so it must be
+    # before email) so if any errors happen importing the rest of the modules they are reported
+    from Email import Email
 
-	# Some BS to shut up the interpreter
-	import logging
-	logging.basicConfig()
-	
-	# Import system modules (mainly used for exiting the program)
-	import os
-	import sys
-	
-	# Import modules for the persistant object database
-	from ZODB import FileStorage, DB
-	import transaction
-	
-	# Import my section information module
-	from Section import Section
+    # Some BS to shut up the interpreter
+    import logging
+    logging.basicConfig()
+    
+    # Import system modules (mainly used for exiting the program)
+    import os
+    import sys
+    
+    # Import modules for the persistant object database
+    from ZODB import FileStorage, DB
+    import transaction
+    
+    # Import my section information module
+    from Section import Section
 except Exception as e:
-	try:
-		report_error(e)
-	except NameError as e:
-		write_message("The email module must not have been able to been imported!")
-	raise e
+    try:
+        report_error(e)
+    except NameError as e:
+        write_message("The email module must not have been able to been imported!")
+    raise e
 
 main()
 sys.exit() # Exit the program
